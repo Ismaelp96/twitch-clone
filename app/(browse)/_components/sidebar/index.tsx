80b05e0795caa52292b6711ed102d17d7e5ac68f
ismaelp96 <ismael6991@live.com>
2023-12-21 09:23:48 -0300

    Hydration error

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,6 +1,6 @@
 import { getRecommended } from "@/lib/recommended-service";
 import { Recommended, RecommendedSkeleton } from "./recommended";
-import { Toggle } from "./toggle";
+import { Toggle, ToggleSkeleton } from "./toggle";
 import { Wrapper } from "./wrapper";
 
 export const SideBar = async () => {
@@ -18,6 +18,7 @@ export const SideBar = async () => {
 export const SidebarSkeleton = () => {
   return (
     <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
+      <ToggleSkeleton />
       <RecommendedSkeleton />
     </aside>
   );
--- app/(browse)/_components/sidebar/toggle.tsx
+++ app/(browse)/_components/sidebar/toggle.tsx
@@ -5,6 +5,7 @@ import { useSidebar } from "@/store/use-sidebar";
 import { Hint } from "@/components/hint";
 
 import { ArrowLeftFromLine, ArrowRightFromLine } from "lucide-react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const Toggle = () => {
   const { collapsed, onExpand, onCollapse } = useSidebar((state) => state);
@@ -24,16 +25,26 @@ export const Toggle = () => {
       {!collapsed && (
         <div className="p-3 pl-6 mb-2 flex items-center w-full">
           <p className="font-semibold text-primary">For you</p>
-
-          <Button
-            onClick={onCollapse}
-            className="h-auto ml-auto"
-            variant="ghost"
-          >
-            <ArrowLeftFromLine className="h-4 w-4" />
-          </Button>
+          <Hint label={label} side="right" asChild>
+            <Button
+              onClick={onCollapse}
+              className="h-auto p-2 ml-auto"
+              variant="ghost"
+            >
+              <ArrowLeftFromLine className="h-4 w-4" />
+            </Button>
+          </Hint>
         </div>
       )}
     </>
+  );
+};
+
+export const ToggleSkeleton = () => {
+  return (
+    <div className="p-3 pl-6 mb-2 hidden lg:flex items-center justify-between w-full">
+      <Skeleton className="h-6 w-[100px]" />
+      <Skeleton className="h-6 w-6" />
+    </div>
   );
 };
