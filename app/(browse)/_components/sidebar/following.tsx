cd4b0b17e7a0f299b9c36e56a42cedd961dea124
ismaelp96 <ismael6991@live.com>
2023-12-22 09:59:47 -0300

    follow list

--- app/(browse)/_components/sidebar/following.tsx
+++ app/(browse)/_components/sidebar/following.tsx
@@ -4,11 +4,13 @@ import { useSidebar } from "@/store/use-sidebar";
 
 import { Follow, User } from "@prisma/client";
 
+import { UserItem, UserItemSkeleton } from "./user-item";
+
 interface FollowingProps {
   data: (Follow & { following: User })[];
 }
 export const Following = ({ data }: FollowingProps) => {
-  const { collapsed } = useSidebar();
+  const { collapsed } = useSidebar((state) => state);
 
   if (!data.length) {
     return null;
@@ -20,6 +22,25 @@ export const Following = ({ data }: FollowingProps) => {
           <p className="text-sm text-muted-foreground">Following</p>
         </div>
       )}
+      <ul className="space-y-2 px-2">
+        {data.map((follow) => (
+          <UserItem
+            key={follow.following.id}
+            username={follow.following.username}
+            imageUrl={follow.following.imageUrl}
+          />
+        ))}
+      </ul>
     </div>
+  );
+};
+
+export const FollowingSkeleton = () => {
+  return (
+    <ul className="px-2 pt-2 lg:pt-0">
+      {[...Array(3)].map((_, i) => (
+        <UserItemSkeleton key={i} />
+      ))}
+    </ul>
   );
 };
--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -4,17 +4,17 @@ import { getFollowedUsers } from "@/lib/follow-service";
 import { Recommended, RecommendedSkeleton } from "./recommended";
 import { Toggle, ToggleSkeleton } from "./toggle";
 import { Wrapper } from "./wrapper";
-import { Following } from "./following";
+import { Following, FollowingSkeleton } from "./following";
 
 export const SideBar = async () => {
   const recommended = await getRecommended();
-  const follows = await getFollowedUsers();
+  const following = await getFollowedUsers();
   return (
     <Wrapper>
       <Toggle />
       <div className="space-y-4 pt-4 lg:pt-0">
         <Recommended data={recommended} />
-        <Following data={follows} />
+        <Following data={following} />
       </div>
     </Wrapper>
   );
@@ -24,6 +24,7 @@ export const SidebarSkeleton = () => {
   return (
     <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
       <ToggleSkeleton />
+      <FollowingSkeleton />
       <RecommendedSkeleton />
     </aside>
   );
